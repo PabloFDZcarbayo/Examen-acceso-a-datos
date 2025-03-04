49baff22f8b39231bd0259d87b5aeb962df9e9f3
Pablo <pablo.fernandez.carbayo@gmail.com>
2025-03-04 11:25:06 +0100

    Pre examen para deploy

---
+++ src/main/java/com/example/Examen_AccesoADatos/ExamenAccesoADatosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Examen_AccesoADatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenAccesoADatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenAccesoADatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Examen_AccesoADatos/ExamenAccesoADatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Examen_AccesoADatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenAccesoADatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
